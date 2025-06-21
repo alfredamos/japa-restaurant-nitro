6bced762fe9ef35708d0557c32b73c815a10cac5
Alfred Amos <alfredamos@gmail.com>
2025-06-21 21:43:06 +0100

    Added same user and admin functionalities.

--- server/api/users/[id].get.ts
+++ server/api/users/[id].get.ts
@@ -1,7 +1,24 @@
+import { StatusCodes } from "http-status-codes";
 import { getUserByIdAction } from "~~/utils/actions/user.action";
+import { useAuth } from "~~/utils/useAuth";
 
 export default defineEventHandler(async (event) => {
   const id = getRouterParam(event, "id");
+
+  //----> Get same user and is-admin flags.
+    const {checkForSameUserAndAdmin} = useAuth();
+    const {isAdmin, isSameUser} = checkForSameUserAndAdmin(id);
+  
+    //----> Check for same user and admin user.
+    if (!isAdmin && !isSameUser){
+      sendError(
+        event,
+        createError({
+          statusCode: StatusCodes.UNAUTHORIZED,
+          statusMessage: "You are not authorized to delete these orders!",
+        })
+      );
+    }
 
   const response = await getUserByIdAction(id);
 
